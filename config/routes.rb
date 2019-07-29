eb832f0476eafb7818271049962ccef22f45a9f8
SimonChee <simon.chee@hotmail.com>
2019-07-29 16:53:25 +0800

    Silly Coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'questions/ask'
+  get 'questions/answer'
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# Prefix Verb URI Pattern       Controller#Action
+#      ask GET  /ask(.:format)    questions#ask
+#   answer GET  /answer(.:format) questions#answer
+
+
+# Rails.application.routes.draw do
+  # get 'questions/ask'
+  # get 'questions/answer'
+#   get 'about', to: 'pages#about', as: :about
+#   get 'contact', to: 'pages#contact', as: :contact
+
+#   # Generic syntax:
+#   # verb 'path', to: 'controller#action', as: :route_name
+# end
